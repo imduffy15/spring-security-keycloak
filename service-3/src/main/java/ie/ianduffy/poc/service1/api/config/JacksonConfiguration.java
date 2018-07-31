b7b0855ebb38182efd75d6917eafed8b11882742
Ian Duffy <ian.duffy@shipyardtech.com>
2018-07-31 20:05:27 +0100

    Add other services
    Signed-off-by: Ian Duffy <ian.duffy@shipyardtech.com>

---
+++ service-3/src/main/java/ie/ianduffy/poc/service1/api/config/JacksonConfiguration.java
@@ -0,0 +1,43 @@
+package ie.ianduffy.poc.service1.api.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.validation.ConstraintViolationProblemModule;
+
+@Configuration
+public class JacksonConfiguration {
+    /*
+     * Jackson Afterburner module to speed up serialization/deserialization.
+     */
+    @Bean
+    public AfterburnerModule afterburnerModule() {
+        return new AfterburnerModule();
+    }
+
+    /*
+     * Module for serialization/deserialization of RFC7807 Problem.
+     */
+    @Bean
+    ProblemModule problemModule() {
+        return new ProblemModule();
+    }
+
+    /*
+     * Module for serialization/deserialization of ConstraintViolationProblem.
+     */
+    @Bean
+    ConstraintViolationProblemModule constraintViolationProblemModule() {
+        return new ConstraintViolationProblemModule();
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer configureJacksonFeatures() {
+        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS, DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+}
